79921bf15953adae371f75dc2c8a5c84ced01899
Sergei Česnakas <10352420+cesnakas@users.noreply.github.com>
2023-06-01 12:57:02 +0400

    Update form

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
+'use strict'
+
 // JS
 import * as bootstrap from 'bootstrap'
 import IMask from 'imask'
 // SCSS
 import '../scss/app.scss'
-import data from "bootstrap/js/src/dom/data";
 
 
 // Nav scroll
@@ -58,7 +59,7 @@ if (imgModal) {
 
 
 // Phone mask
-const maskFormPhone = document.getElementById('formPhone')
+const maskFormPhone = document.getElementById('staticContact')
 const maskModalPhone = document.getElementById('modalPhone')
 const maskOptions = {
   mask: '+{7} ({9}00) 000-00-00'
@@ -76,39 +77,59 @@ Array.from(forms).forEach(form => {
       event.stopPropagation()
     }
     form.classList.add('was-validated')
+    document.querySelector('#staticFormFeedback').addEventListener('submit', staticFormFeedback, false)
   }, false)
 })
 
-// Form
-document.getElementById('formModal').addEventListener('submit', postAjax)
-function postAjax(e) {
-  e.preventDefault()
 
+// Form
+const formModal = new bootstrap.Modal(document.querySelector('#formModal'))
+const successModal = new bootstrap.Modal(document.querySelector('#formSuccess'))
+document.getElementById('modalFormFeedback').addEventListener('submit', function(e) {
   let xhr = new XMLHttpRequest()
   let f = this
+  e.preventDefault()
 
   xhr.open('POST', 'https://form-perm.ru/mail.php', true)
-
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-  xhr.send('nameFF=' + this.nameFF + '&contactFF=' + this.contactFF + '&agreeFF=' + this.agreeFF)
 
   xhr.onreadystatechange = function () {
-
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // console.log(xhr.responseText)
-      alert(xhr.responseText + ', Ваше сообщение получено.\nБлагодарим за проявленный интерес!\nНаши специалисты ответят Вам в ближайшее время.')
-      f.contactFF.removeAttribute('value')
-      f.contactFF.value = ''
-
-    } else if (xhr.onerror === 404) {
-      // console.log('Error')
-      alert('Извините, данные не были переданы')
-    }
-
-    xhr.onerror = function () {
-      alert('Извините, данные не были переданы');
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log(xhr.responseText)
+      setTimeout(function() {
+        formModal.hide()
+      }, 500)
+      setTimeout(function() {
+        successModal.show()
+        // f.nameFF.removeAttribute('value')
+        // f.nameFF.value = ''
+        // f.contactFF.removeAttribute('value')
+        // f.contactFF.value = ''
+      }, 1500)
     }
   }
-  // xhr.send(new FormData(e.target))
-  return false
+
+  xhr.send('nameFF=' + f.nameFF.value + '&contactFF=' + f.contactFF.value + '&agreeFF=' + f.agreeFF.value)
+
+}, false)
+
+
+function staticFormFeedback(e) {
+  let xhr = new XMLHttpRequest()
+  let f = this
+  e.preventDefault()
+  xhr.open('POST', 'https://form-perm.ru/mail.php', true)
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+  xhr.send('staticNameFF=' + f.staticNameFF.value + '&staticContactFF=' + f.staticContactFF.value + '&staticCheckFF=' + f.staticCheckFF.value)
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log(xhr.responseText)
+      setTimeout(function() {
+        successModal.show()
+      }, 1500)
+      document.querySelector('#staticFormFeedback').reset()
+    }
+  }
 }
+
+
--- src/mail.php
+++ src/mail.php
@@ -1,11 +1,10 @@
 <?php
 
-//if (array_key_exists('messageFF', $_POST)) {
-if (isset($_POST["contactFF"])) {
+if (!empty($_POST["nameFF"]) && !empty($_POST["contactFF"])) {
   $to = "s.cesnakas@yandex.ru";
   $subject = "Заполнена контактная форма с " . $_SERVER['HTTP_REFERER'];
   $subject = "=?utf-8?b?" . base64_encode($subject) . "?=";
-  $message = "Имя: " . $_POST["nameFF"] . "\nТелефон: " . $_POST["contactFF"] . "\nIP: " . $_SERVER['REMOTE_ADDR'] . "\nСообщение: " . $_POST["messageFF"];
+  $message = "Имя: " . $_POST["nameFF"] . "\nТелефон: " . $_POST["contactFF"] . "\nIP: " . $_SERVER['REMOTE_ADDR'] . "\nAgreement: " . $_POST["agreeFF"];
   $headers = 'Content-type: text/plain; charset="utf-8"';
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Date: " . date('D, d M Y h:i:s O') . "\r\n";
